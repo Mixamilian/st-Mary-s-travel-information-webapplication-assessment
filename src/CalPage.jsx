13014de243906362054c53be41c477b75d015873
Mixamilian <2203mv@gmail.com>
2026-05-07 12:37:00 +0100

    Add files via upload
    Created the Calculate Fare page with simple obvious steps that ask for the travel type( Train or bus) and the distance that you want to travel before outputting a total fare cost. this calculation only uses predefined multipliers for Train and Bus to be multiplied by the Distance to get a total.

--- src/CalPage.jsx
+++ src/CalPage.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
 import './App.css'
 
 function CalPage(){
 
+    const[option, setTravel] = useState("");
+    const[distance, setDistance] = useState("");
+    const[cost, setCost] = useState("0");
+
+    const calFare = () => {
+
+        const out = Number(distance) * Number(option);
+        setCost(out);
+
+        
+
+    }
+    
     return(
         <div>
             <div className = "title">
                 <h1>Calculate Fare</h1>
+           </div>
+           <forum className = "fare">
+            <label for = "travelType">Select a travel Type--</label>
+            <select value={option} onChange={(e) => setTravel(e.target.value)}>
+                <option value = "0" selected >Select Box</option>
+                <option value = "3">Train</option>
+                <option value = "2">Bus</option>
+            </select>
+            <label>Enter distance in km--</label>
+            <input type = "number" value = {distance} onChange = {(e) => setDistance(e.target.value)}></input>
+            <button onClick = {calFare}>Submit</button>
+           </forum>
+           <div className = "output">
+                <p>Cost of Fare is £{cost}</p>
            </div>
         </div>
     )
