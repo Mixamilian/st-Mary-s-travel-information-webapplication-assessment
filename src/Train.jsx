177cd790f055d941b0b37b6e84a0e35056e800e4
Mixamilian <2203mv@gmail.com>
2026-05-07 19:35:37 +0100

    Add files via upload
    Completed and Integrated Content for the Train Information PAge with Links to offical government websites for further information. Testing was successfull to load when state of the button was changed.

--- src/Train.jsx
+++ src/Train.jsx
@@ -12,9 +12,9 @@ function TrainPage(){
 
                     <p>The government southern western railway train network is a fantasic method of travel through and from twickenham with many connected jorneys that link with twickenham. It is a fast and mostly reliable method of getting in and out of london connecting places as far as reading to London waterloo and from there many other networks in the North, east and south also.</p>
                     <h4>Benefits</h4>
-                    <p></p>
+                    <p>The Main Benefits of this Railway System connecting to Twickenham is its reliable fast and cost-predictable services that allow for Travel over a long Distance with fairs being on average 0.65p per Kilometer with Some Inaccuracey due to London Time fares and zone which casue shorter trips such as Feltam to twickenham Costing £4.20 for a single whereas longer trips out side of London like Aldershot to Twickenham costing £17.40. On of the Main Benefits of Twickenham Station is its Fast access to central London with Many Trains operating on different Lines Connecting to London Waterloo running every 5-20 minutes.</p>
                     <h4>Limitations</h4>
-                    <p></p>
+                    <p>Due to the popularity of this service Twickenham Especially often sees Overcrowding during peak-times and knock-on delays from this disruption. This can make the comfort of this Long jorney feel stressful and less flexible. Similar Limitations are fragile reliability during Distruptions as many Trains operate on this single line causeing delay and recovery time of up to 1 hour which isnt promising for students and employee commuters. </p>
                     <a href="https://www.southwesternrailway.com/" target = "_blank"> Visit Southern Western Railway official website for more information</a>
 
                 </div>
